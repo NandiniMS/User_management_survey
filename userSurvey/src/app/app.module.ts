5a935226429a2e0b3b666431665fd658cabc2e6d
NandiniMS <48900380+NandiniMS@users.noreply.github.com>
2021-03-17 23:39:27 +0530

    Initial upload - UserSurvey
    Commit after uploading the user survey app

---
+++ userSurvey/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationFormComponent,
+    LoginFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
